63386d016be53b6b4db38e973fac4e3960e10bb9
fenguel <felipedelima@hotmail.com>
2022-08-05 18:45:30 -0300

    ok

---
+++ scr/app.ts
@@ -0,0 +1,91 @@
+// // DESAFIO 1
+
+let employee = {
+    code: 10,
+    name: 'John'
+};
+
+employee.code = 10;
+employee.name = "John";
+
+
+
+// DESAFIO 2
+
+enum Trabalho {
+    Atriz,
+    Padeiro
+}
+
+type Humano = {
+    nome: string,
+    idade: number,
+    profissao: Trabalho
+}
+
+let pessoa1: Humano = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa2: Humano = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Trabalho.Padeiro
+};
+
+let pessoa3: Humano = {
+    nome: 'laura',
+    idade: 32,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa4: Humano = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Trabalho.Padeiro
+}
+
+
+
+// DESAFIO 3
+
+export {}
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+
+let saldoTotal = 0
+
+limparSaldo()
+
+function somarAoSaldo(soma: number) {
+    if (campoSaldo) {
+        saldoTotal += soma
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', () => {
+        somarAoSaldo(Number(soma.value)); 
+    });
+}
+botaoLimpar.addEventListener('click', () => { // Percebam que aqui o typescript não acusou o botao de ser nulo e não precisei do if. Caso queiram fazer o teste, retirem a exclamação.
+    limparSaldo();
+});
